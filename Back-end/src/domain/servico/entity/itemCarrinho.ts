d8857c6600c47bb3c63cff4adb118bfb00f5100c
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-07-21 13:13:35 -0300

    modelagem da entity itemCarrinho

---
+++ Back-end/src/domain/servico/entity/itemCarrinho.ts
@@ -0,0 +1,39 @@
+import { Servico } from "./servico"
+
+export type itemCarrinhoProps = {
+    servico: Servico,
+    quantidade: number
+}
+
+export class itemCarrinho {
+    private constructor(private readonly props:itemCarrinhoProps) {}
+
+    public static create(servico: Servico, quantidade: number): itemCarrinho {
+        return new itemCarrinho({
+            servico, quantidade
+        })
+    }
+    public static with(props:itemCarrinhoProps){
+        return new itemCarrinho(props)
+    }
+    public get servico(): Servico {
+        return this.props.servico;
+    }
+
+    public get quantidade(): number {
+        return this.props.quantidade;
+    }
+
+
+    public updateQuantidade(novaQuantidade: number): void {
+        if (novaQuantidade <= 0) {
+            throw new Error("A nova quantidade deve ser maior que zero.");
+        }
+        this.props.quantidade = novaQuantidade;
+    }
+
+
+    public get subtotal(): number {
+        return this.props.servico.preco * this.props.quantidade;
+    }
+}
