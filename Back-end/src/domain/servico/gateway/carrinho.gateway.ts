4e42189eeecf201f8bf5e1c502c01a726d2e75ea
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-07-25 21:39:03 -0300

    como não sera um e-commerce comum retirei a logica de ccarrinho e vou começar a lógica de agendamento

--- Back-end/src/domain/servico/gateway/carrinho.gateway.ts
+++
@@ -1,9 +0,0 @@
-import { Carrinho } from "../entity/carrinho";
-
-
-export interface CarrinhoGateway {
-    save(carrinho: Carrinho): Promise<void>
-    findById(id: string): Promise<Carrinho | null>;
-    delete(id:string): Promise<void>
-    list(): Promise<Carrinho[]>
-}
