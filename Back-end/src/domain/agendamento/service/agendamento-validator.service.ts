4ff59f477c23db762e9d4d41d253274d42f32cdb
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-08-14 13:27:40 -0300

    validator de Agendamentos concluido

--- Back-end/src/domain/agendamento/service/agendamento-validator.service.ts
+++ Back-end/src/domain/agendamento/service/agendamento-validator.service.ts
@@ -1,4 +1,4 @@
-import { Agendamento } from "@prisma/client";
+import { Agendamento } from "../entity/agendamento";
 import { AgendamentoGateway } from "../gateway/agendamento.gateway";
 import { AgendamentoValidator } from "./agendamento-validator.interface";
 
@@ -13,6 +13,12 @@ export class AgendamentoValidatorService implements AgendamentoValidator{
                 agendamento.horaInicio,
                 agendamento.horaFim
             )
+            const conflitoEncontrado = agendamentosConflitantes.some(
+                (conflitante) => agendamento.estaEmConflitoCom(conflitante)
+            )
+            if(conflitoEncontrado) {
+                throw new Error("Já existe um agendamento nesse horario")
+            }
         }catch(error: any) {
             throw new Error("houve um erro ao validar conflitos", error)
         }
