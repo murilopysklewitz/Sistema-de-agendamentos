073866fa54ca9a690f50bc87e604a27579868f21
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-08-14 13:15:20 -0300

    mudei as pastas para separar de servico

--- Back-end/src/domain/servico/gateway/agendamento.gateway.ts
+++
@@ -1,8 +0,0 @@
-import { Agendamento } from "../entity/agendamento";
-
-export interface AgendamentoGateway {
-    save(agendamento: Agendamento): Promise<Agendamento>
-    list(): Promise<Agendamento[]>
-    findById(id: string): Promise<Agendamento | null>
-    findByInterval(data: Date, horaInicio: Date, horaFim: Date): Promise<Agendamento[]>
-}
--- Back-end/src/domain/servico/gateway/cliente.gateway.ts
+++
@@ -1,9 +0,0 @@
-import { Cliente } from "../entity/cliente";
-
-export interface ClienteGateway {
-    save(cliente: Cliente): Promise<void>
-    list(): Promise<Cliente[]>
-    findById(id: string): Promise<Cliente>
-    delete(id: string): Promise<void>
-    update(cliente: Cliente): Promise<void> 
-}
