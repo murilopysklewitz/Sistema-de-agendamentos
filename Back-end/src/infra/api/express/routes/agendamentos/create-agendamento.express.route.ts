8678e80ed3784243bb7d2d11b8879d0ab3b5995e
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-08-27 16:26:50 -0300

    construi a rota Create de agendamentos

--- Back-end/src/infra/api/express/routes/agendamentos/create-agendamento.express.route.ts
+++ Back-end/src/infra/api/express/routes/agendamentos/create-agendamento.express.route.ts
@@ -1,9 +1,10 @@
 import { AgendamentoStatus } from "src/domain/agendamento/entity/agendamento";
 import { ServicoProps } from "src/domain/servico/entity/servico";
 import { HttpMethod, Route } from "../routes";
-import { CreateAgendamentoUsecase } from "src/usecases/agendamento.usecases/create-agendamento/createAgendamento.usecase";
+import { CreateAgendamentoInputDto, CreateAgendamentoUsecase } from "src/usecases/agendamento.usecases/create-agendamento/createAgendamento.usecase";
+import { Request, Response } from "express";
 
-export type createAgendamentoRouteResponseDto = {
+export type CreateAgendamentoResponseDto = {
             id: string;
             clienteId: string;
             servico: ServicoProps;
@@ -15,11 +16,43 @@ export type createAgendamentoRouteResponseDto = {
 export class CreateAgendamentoRoute implements Route {
     private constructor(
         private readonly path:string,
-        private readonly httpMethod: HttpMethod,
+        private readonly method: HttpMethod,
         private readonly createAgendamentoService: CreateAgendamentoUsecase,
     ){}
 
     public static create(createAgendamentoService: CreateAgendamentoUsecase) {
-        
+        return new CreateAgendamentoRoute('/api/agendamento', HttpMethod.POST, createAgendamentoService)
     }
+
+    public  getHandler() {
+        return async (request: Request, response: Response) => {
+            const { clienteId, servico, data, horaInicio,} = request.body;
+
+            const input: CreateAgendamentoInputDto = {
+                clienteId, servico, data, horaInicio
+            }
+            const output: CreateAgendamentoResponseDto = await this.createAgendamentoService.execute(input);
+            const responseBody = this.present(output);
+        }
+    }
+               public getPath():string {
+                return this.path
+            }
+        
+            public getMethod(): HttpMethod {
+                return this.method;
+            }
+        
+            private present(input:CreateAgendamentoResponseDto): CreateAgendamentoResponseDto {
+                const response = {
+                    id: input.id,
+                    clienteId: input.clienteId,
+                    servico: input.servico,
+                    status: input.status,
+                    data: input.data,
+                    horaInicio: input.horaInicio,
+                    horaFim: input.horaFim,
+                }
+                return response;
+            }
 }
