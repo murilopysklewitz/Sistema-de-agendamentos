622c2cb7803113f8af620fdde53fe31134f4d408
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-02 14:32:11 -0300

    mudei a promise do método save para aceitar o retorno de string e para que findById não retorne null

--- Back-end/src/domain/servico/gateway/servico.gateway.ts
+++ Back-end/src/domain/servico/gateway/servico.gateway.ts
@@ -1,9 +1,9 @@
 import { Servico } from "../entity/servico";
 
 export interface ServicoGateway {
-    save(servico: Servico): Promise<void>;
+    save(servico: Servico): Promise<string>;
     list(): Promise<Servico[]>;
-    findById(id: string): Promise<Servico | null>;
+    findById(id: string): Promise<Servico>;
     delete(id: string): Promise<void>;
     update(servico:Servico): Promise<void>;
 }
