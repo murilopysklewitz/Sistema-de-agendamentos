ccd22ddd49b0bedffe6a75a81f26cca2351a82e3
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-10 15:14:24 -0300

    documentação centralizada e swagger configurado

--- Back-end/src/config/swagger.ts
+++ Back-end/src/config/swagger.ts
@@ -1,26 +1,11 @@
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
 import { Express } from "express";
-
-const options: swaggerJSDoc.Options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "API meu pet feliz",
-      version: "1.0.0",
-      description: "Documentação da API com Swagger + TypeScript",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000", 
-      },
-    ],
-  },
-  apis: ["./src/infra/api/express/routes/**/*.ts"], 
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import path from "path";
 
 export function setupSwagger(app: Express) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  const swaggerPath = path.resolve(__dirname, "./swagger.yaml");
+  const swaggerDocument = yaml.load(swaggerPath);
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
