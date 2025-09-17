8337d385249c7b637d5f0c2d3a6dd5a662308ab8
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-17 15:23:12 -0300

    mudança para save gerar promise de string(retornar o id)

--- Back-end/src/domain/cliente/gateway/cliente.gateway.ts
+++ Back-end/src/domain/cliente/gateway/cliente.gateway.ts
@@ -1,7 +1,7 @@
 import { Cliente } from "../entity/cliente";
 
 export interface ClienteGateway {
-    save(cliente: Cliente): Promise<void>
+    save(cliente: Cliente): Promise<string>
     list(): Promise<Cliente[]>
     findById(id: string): Promise<Cliente>
     delete(id: string): Promise<void>
