e2b015b5797c1e6d1431fd82d27aab69cabba53b
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-24 11:21:28 -0300

    feat(cliente): rota de listar clientes configurada

---
+++ Back-end/src/infra/api/express/routes/clientes/list-cliente.express.route.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express"
+import { ListClienteUsecase } from "../../../../../usecases/cliente.usecases/listCliente.usecase"
+import { HttpMethod, Route } from "../routes"
+
+export type ListClienteResponseDTO = {
+    clientes:{
+        id: string,
+        nome: string,
+        email: string,
+        numero: string
+    }[]
+}
+
+export class LisstClienteRoute implements Route {
+    private constructor(private readonly path:string, private readonly method:HttpMethod, private readonly listClienteService: ListClienteUsecase){}
+
+    public static create(listClienteService: ListClienteUsecase){
+        return new LisstClienteRoute("/api/clientes", HttpMethod.GET, listClienteService)
+    }
+
+    public getHandler() {
+        return async (request: Request, response: Response) => {
+            const output = await this.listClienteService.execute()
+
+            const responseBody = this.present(output)
+            response.status(200).json(responseBody)
+        }
+    }
+
+    public present(input: ListClienteResponseDTO): ListClienteResponseDTO {
+        const response: ListClienteResponseDTO = {
+            clientes: input.clientes.map((cliente) => ({
+                id: cliente.id,
+                nome: cliente.nome,
+                email: cliente.email,
+                numero: cliente.numero
+            }))
+        }
+        return response
+    }
+
+    public getPath(): string {
+        return this.path    
+    }
+    public getMethod(): HttpMethod {
+        return this.method
+    }
+
+}
