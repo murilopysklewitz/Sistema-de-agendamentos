637aa32607b22241b4a387205fae765a3ac17c67
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-29 13:32:02 -0300

    adicionei o método de findByEmail no gateway de cliente

--- Back-end/src/domain/cliente/gateway/cliente.gateway.ts
+++ Back-end/src/domain/cliente/gateway/cliente.gateway.ts
@@ -4,6 +4,7 @@ export interface ClienteGateway {
     save(cliente: Cliente): Promise<string>
     list(): Promise<Cliente[]>
     findById(id: string): Promise<Cliente>
+    findByEmail(email: string): Promise<Cliente>
     delete(id: string): Promise<void>
     update(cliente: Cliente): Promise<void> 
 }
