1bc440b90265133c1fb9c88f2a2d142462fa14e3
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-02 12:12:56 -0300

    finalizei a rota de login de clientes

---
+++ Back-end/src/infra/api/express/routes/clientes/login-cliente.express.route.ts
@@ -0,0 +1,34 @@
+import { LoginClienteUsecase } from "../../../../../usecases/cliente.usecases/loginCliente.usecase"
+import { HttpMethod, Route } from "../routes"
+import { Request, Response } from "express"
+
+export class LoginClienteRoute implements Route {
+    private constructor(
+        private readonly path: string,
+        private readonly httpMethod: HttpMethod,
+        private readonly loginClienteService: LoginClienteUsecase){}
+
+    public static create(path: string, httpMethod: HttpMethod, loginClienteService: LoginClienteUsecase) {
+        return new LoginClienteRoute(
+            "/api/cliente/auth",
+            HttpMethod.POST,
+            loginClienteService
+        )
+    }
+    public getHandler() {
+        return async (request: Request, response: Response) => {
+            const {email, senha} = request.body
+            const output = await this.loginClienteService.execute({email, senha})
+            
+            response.status(200)
+        }
+    }
+
+    public getPath(): string {
+        return this.path
+    }
+    public getMethod(): HttpMethod {
+        return this.httpMethod
+    }
+
+}
