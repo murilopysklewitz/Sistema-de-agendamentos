97166edc8ac952078f6954dbe3bb10458665b2de
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-02 22:27:00 -0300

    adaptação das rotas

---
+++ Back-end/dist/infra/api/express/routes/clientes/login-cliente.express.route.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LoginClienteRoute = void 0;
+const routes_1 = require("../routes");
+class LoginClienteRoute {
+    path;
+    httpMethod;
+    loginClienteService;
+    constructor(path, httpMethod, loginClienteService) {
+        this.path = path;
+        this.httpMethod = httpMethod;
+        this.loginClienteService = loginClienteService;
+    }
+    static create(loginClienteService) {
+        return new LoginClienteRoute("/api/cliente/auth", routes_1.HttpMethod.POST, loginClienteService);
+    }
+    getHandler() {
+        return async (request, response) => {
+            const { email, senha } = request.body;
+            const output = await this.loginClienteService.execute({ email, senha });
+            response.status(200);
+        };
+    }
+    getPath() {
+        return this.path;
+    }
+    getMethod() {
+        return this.httpMethod;
+    }
+}
+exports.LoginClienteRoute = LoginClienteRoute;
