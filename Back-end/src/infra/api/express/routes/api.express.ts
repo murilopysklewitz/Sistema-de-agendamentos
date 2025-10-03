da00653474acbd349a907b129e996a3c98f205f2
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-02 22:26:50 -0300

    implementei o middleware como opcional para criação das rotas

--- Back-end/src/infra/api/express/routes/api.express.ts
+++ Back-end/src/infra/api/express/routes/api.express.ts
@@ -1,5 +1,5 @@
 import { Api } from "../../api";
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import { Route } from "./routes";
 import { setupSwagger } from "../../../../config/swagger";
 
@@ -10,9 +10,24 @@ export class ApiExpress implements Api {
     private constructor(routes: Route[]) {
         this.app = express()
         this.app.use(express.json())
+
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
+            console.log(`📨 ${req.method} ${req.path}`);
+            next();
+        });
+
         this.addRoutes(routes)
 
         setupSwagger(this.app)
+
+        this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+            console.error('💥 Erro capturado:', err)
+            console.error('Stack:', err.stack);
+            res.status(500).json({ 
+                message: "Erro interno do servidor",
+                error: err.message 
+            });
+        });
     }
 
 
@@ -20,12 +35,16 @@ export class ApiExpress implements Api {
         return new ApiExpress(routes)
     }
 
+
+
     private addRoutes(routes: Route[]) {
         routes.forEach((route) => {
             const path = route.getPath()
             const method = route.getMethod()
             const handler = route.getHandler()
-            const middlewares = route.getMiddlewares ? route.getMiddlewares(): []
+            const middlewares = route.getMiddlewares?.() ?? []
+
+            console.log(`🛣️  Registrando rota: ${method.toUpperCase()} ${path} - Middlewares: ${middlewares.length}`)
 
             if(middlewares.length > 0){
                 this.app[method](path, ...middlewares.map(m => m.handle.bind(m)), handler)
@@ -38,6 +57,7 @@ export class ApiExpress implements Api {
     public start(port:number) {
         this.app.listen(port, () => {
             console.log(`Server running on port ${port}`)
+            console.log(`📚 Swagger docs: http://localhost:${port}/api-docs`)
         })
     }
 }
