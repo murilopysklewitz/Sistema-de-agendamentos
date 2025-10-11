60459eacdf4ecd1a0da5e11819a5c3c99a0f2e32
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-10-11 12:42:35 -0300

    adicionando logs descritivos

--- Back-end/src/infra/api/express/routes/clientes/findByEmail.express.route.ts
+++ Back-end/src/infra/api/express/routes/clientes/findByEmail.express.route.ts
@@ -20,8 +20,12 @@ export type FindByEmailResponseDto = {
 
         public getHandler() {
             return async (request: Request, response: Response) => {
+                console.log(`Request body: ${JSON.stringify(request.body)}`)
+                console.log(`Request params: ${JSON.stringify(request.params)}`)
                 const {email} = request.params
+                console.log(`Request para achar cliente ${email}`)
                 const cliente = await this.findByEmailClientesService.execute({email})
+                console.log(`Response body: ${JSON.stringify(cliente)}`)
                 response.status(200).json(cliente)
             }
         }
